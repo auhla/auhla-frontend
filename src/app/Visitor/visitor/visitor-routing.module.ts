b4d40ab5a31fdf0ba1da7adb6876ee97c333b09f
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-09 17:43:54 -0400

    refactor(visitor): child components

--- src/app/Visitor/visitor/visitor-routing.module.ts
+++ src/app/Visitor/visitor/visitor-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeScreenComponent } from './screens/home-screen/home-screen.component';
+import { VisitorComponent } from './visitor.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeScreenComponent }
+  { 
+    path: '',
+    component: VisitorComponent,
+    children: [
+      { path: '', component: HomeScreenComponent }
+    ]
+  }
 ];
 
 @NgModule({
