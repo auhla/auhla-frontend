9cecb1c8f4eff46d0ce9fd2884042592c495da2b
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-09 17:40:48 -0400

    feat(visitor): import navbar

--- src/app/Visitor/visitor/visitor.module.ts
+++ src/app/Visitor/visitor/visitor.module.ts
@@ -4,10 +4,15 @@ import { CommonModule } from '@angular/common';
 import { VisitorRoutingModule } from './visitor-routing.module';
 import { VisitorComponent } from './visitor.component';
 import { HomeScreenComponent } from './screens/home-screen/home-screen.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
-  declarations: [VisitorComponent, HomeScreenComponent],
+  declarations: [
+    VisitorComponent, 
+    HomeScreenComponent,
+    NavbarComponent
+  ],
   imports: [
     CommonModule,
     VisitorRoutingModule
