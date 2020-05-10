8959f1f9644c65a7c2d0dd6c62a0e47d41a7cc0a
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-09 22:45:31 -0400

    feat(shared): workshops card component

---
+++ src/app/Core/shared/components/workshops-card/workshops-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-workshops-card',
+  templateUrl: './workshops-card.component.html',
+  styleUrls: ['./workshops-card.component.css']
+})
+export class WorkshopsCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
