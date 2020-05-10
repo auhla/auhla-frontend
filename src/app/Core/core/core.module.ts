66d63d05e7aeeffffee900ed1a1cbdc772310936
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-10 11:05:20 -0400

    feat(core): firebase module

--- src/app/Core/core/core.module.ts
+++ src/app/Core/core/core.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FirebaseModule } from './modules/firebase/firebase.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    FirebaseModule
   ],
   providers: [
 
