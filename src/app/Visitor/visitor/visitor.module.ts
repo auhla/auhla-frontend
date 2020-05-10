0b326497d6ca2090bafba723f929763ebf2313e6
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-09 22:46:06 -0400

    feat(visitor): footer component

--- src/app/Visitor/visitor/visitor.module.ts
+++ src/app/Visitor/visitor/visitor.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { VisitorRoutingModule } from './visitor-routing.module';
+
+import { SharedModule } from 'src/app/Core/shared/shared.module';
+
 import { VisitorComponent } from './visitor.component';
 import { HomeScreenComponent } from './screens/home-screen/home-screen.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     VisitorComponent, 
     HomeScreenComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
-    VisitorRoutingModule
+    VisitorRoutingModule,
+    SharedModule
   ]
 })
 export class VisitorModule { }
