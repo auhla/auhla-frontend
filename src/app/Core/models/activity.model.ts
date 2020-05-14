e12d890f8fa1f3b3cf11620407be496b4bb5c4d5
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-13 22:03:25 -0400

    feat(models): activity model

---
+++ src/app/Core/models/activity.model.ts
@@ -0,0 +1,10 @@
+export interface Activity {
+  id: string;
+  key: string;
+  name: string;
+  subject: string;
+  unit: string;
+  description: string;
+  learninObjectives: number[];
+  loot: number;
+};
