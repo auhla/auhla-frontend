c03c3e0f1c0a60263635e99abd80287e18950a4c
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-13 22:11:47 -0400

    feat(shared): join method

--- src/app/Core/shared/components/join-activity-card/join-activity-card.component.ts
+++ src/app/Core/shared/components/join-activity-card/join-activity-card.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NotificationService } from '@services/notification/notification.service';
+import { ActivityProviderService } from '@core/providers/activity/activity-provider.service';
+import { ActivityService } from '@activity/services/activity/activity.service';
+import { Activity } from '@models/activity.model';
 
 @Component({
   selector: 'shared-join-activity-card',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JoinActivityCardComponent implements OnInit {
 
-  constructor() { }
+  key: string;
+  name: string;
+
+  constructor(
+    private activityProvider: ActivityProviderService,
+    private activityService: ActivityService,
+    private notificationService: NotificationService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  async join(key: string, name: string){
+    try {
+      const activity: Activity = await this.activityProvider.getActivityByKey(key);
+      
+      this.activityService.handleActivity(activity);
+      this.router.navigate(['student/activity/start'],);
+    } 
+    catch (error) {
+      this.notificationService.error('La llave es incorrecta');
+    }
+
+  }
 }
