5b8d2fa58de959561c7e261aaf9269be8b0a7d66
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-15 21:40:28 -0400

    feat(workshop): timeline route

--- src/app/Student/workshop/workshop-routing.module.ts
+++ src/app/Student/workshop/workshop-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WorkshopComponent } from './workshop.component';
+import { TimelineScreenComponent } from './screens/timeline-screen/timeline-screen.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '',
+    component: WorkshopComponent,
+    children: [
+      { path: 'timeline', component: TimelineScreenComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
